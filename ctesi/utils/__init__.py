da21db0253169095b48fdc73800ea0106e8ef868
Radu Suciu <radusuciu@gmail.com>
2017-05-26 13:52:47 -0700

    Expand cimageparams class to support chem.table

--- ctesi/utils/__init__.py
+++ ctesi/utils/__init__.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict, namedtuple
+import csv
 
 VALID_NUCLEIC_ACIDS = 'ACGTNUKSYMWRBDHV'
 VALID_AMINO_ACIDS = 'APBQCRDSETFUGVHWIYKZLXMN'
@@ -37,9 +38,37 @@ class CimageParams:
 
     def __init__(self, path=None, data=None):
         self.data = data or OrderedDict()
+        self._light = None
+        self._heavy = None
 
         if path:
             self.read_file(path)
+
+
+    @property
+    def light(self):
+        if self._light is None:
+            self.parse_chem_tables()
+
+        return self._light
+
+
+    @light.setter
+    def light(self, light):
+        self._light = light
+
+
+    @property
+    def heavy(self):
+        if self._heavy is None:
+            self.parse_chem_tables()
+            
+        return self._heavy
+
+
+    @heavy.setter
+    def heavy(self, heavy):
+        self._heavy = heavy
 
 
     def read(self, raw):
@@ -53,6 +82,8 @@ class CimageParams:
                 (key, value) = map(str.strip, line.split('='))
                 self.data[key] = self.CimageParam(comment, key, value)
 
+        self.parse_chem_tables()
+
 
     def read_file(self, path):
         with open(str(path)) as f:
@@ -60,6 +91,42 @@ class CimageParams:
 
         self.read(raw)
         return self
+
+
+    def parse_chem_tables(self):
+        self.light = self._parse_chem_table(self.data['light.chem.table'].value)
+        self.heavy = self._parse_chem_table(self.data['heavy.chem.table'].value)
+        return self
+
+
+    def _parse_chem_table(self, path):
+        table = OrderedDict()
+
+        with open(path) as f:
+            reader = csv.reader(f, delimiter='\t')
+            headers = next(reader)[1:]
+
+            for row in reader:
+                if row:
+                    table[row[0]] = OrderedDict(zip(headers, row[1:]))
+
+        return table    
+
+    def write_chem_tables(self, light_path=None, heavy_path=None):
+        self._write_chem_table(light_path or self['light.chem.table'], self.light)
+        self._write_chem_table(heavy_path or self['heavy.chem.table'], self.heavy)
+        return self
+
+
+    def _write_chem_table(self, path, data):
+        with open(path, 'w', newline='') as f:
+            writer = csv.writer(f, delimiter='\t', quoting=csv.QUOTE_NONE)
+
+            headers = [''] + list(data['A'].keys())
+            writer.writerow(headers)
+
+            for aa, counts in data.items():
+                writer.writerow([aa] + list(counts.values()))
 
 
     def to_string(self):
