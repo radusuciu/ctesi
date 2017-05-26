850be0e90c490a16d090604601cf4130f92d464b
Radu Suciu <radusuciu@gmail.com>
2017-05-26 12:40:44 -0700

    Utility class for reading and writing cimageparams

--- ctesi/utils/__init__.py
+++ ctesi/utils/__init__.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict, namedtuple
 
 VALID_NUCLEIC_ACIDS = 'ACGTNUKSYMWRBDHV'
 VALID_AMINO_ACIDS = 'APBQCRDSETFUGVHWIYKZLXMN'
@@ -26,3 +27,67 @@ def validate_search_params(search_params):
             valid_mods.append(mod)
 
     return { 'diff_mods': valid_mods }
+
+
+
+class CimageParams:
+
+    CimageParam = namedtuple('CimageParam', ['comment', 'key', 'value'])
+    _comment_char = '!'
+
+    def __init__(self, path=None, data=None):
+        self.data = data or OrderedDict()
+
+        if path:
+            self.read_file(path)
+
+
+    def read(self, raw):
+        # reset rather than append
+        self.data = OrderedDict()
+
+        for line in raw:
+            if line.startswith(self._comment_char):
+                comment = line.strip(self._comment_char + ' ')
+            else:
+                (key, value) = map(str.strip, line.split('='))
+                self.data[key] = self.CimageParam(comment, key, value)
+
+
+    def read_file(self, path):
+        with open(str(path)) as f:
+            raw = f.read().splitlines()
+
+        self.read(raw)
+        return self
+
+
+    def to_string(self):
+        lines = []
+
+        for item in self.data:
+            param = self.data[item]
+
+            lines.append('{} {} {}\n{} = {}'.format(
+                self._comment_char * 3,
+                param.comment,
+                self._comment_char*3,
+                param.key,
+                param.value
+            ))
+
+        return '\n'.join(lines)
+
+
+    def write_file(self, path):
+        if not self.data:
+            return
+
+        with open(str(path), 'w') as f:
+            f.write(self.to_string())
+
+        return self
+
+
+    def __getitem__(self, key):
+        return self.data[key].value
