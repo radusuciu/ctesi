7b14bac63fd0d62bb74d6eff6c744e871a8df3fd
Radu Suciu <radusuciu@gmail.com>
2017-06-19 15:04:30 -0700

    Unzip quietly

--- ctesi/utils/ctesi-dl.sh
+++ ctesi/utils/ctesi-dl.sh
@@ -2,5 +2,5 @@
 
 url=$1
 filename=$(curl -JO "$url" | grep -o -E 'filename\s'\''(.*)'\''$' | sed -e 's/filename //' -e 's/'\''//g')
-unzip -d "${filename%*.zip}" "$filename"
+unzip -q -d "${filename%*.zip}" "$filename"
 rm "$filename"
