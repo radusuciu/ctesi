59acf8d2755da2b9dcbe1208e106795f7a3bfb09
Radu Suciu <radusuciu@gmail.com>
2017-06-19 15:09:24 -0700

    Exit dl script on error

--- ctesi/utils/ctesi-dl.sh
+++ ctesi/utils/ctesi-dl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 url=$1
 filename=$(curl -JO "$url" | grep -o -E 'filename\s'\''(.*)'\''$' | sed -e 's/filename //' -e 's/'\''//g')
 unzip -q -d "${filename%*.zip}" "$filename"
