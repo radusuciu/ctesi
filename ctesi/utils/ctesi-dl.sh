a1009203c803a78cd932659783e018286b97910f
Radu Suciu <radusuciu@gmail.com>
2017-06-19 15:16:57 -0700

    Flesh out zip dl script some more

--- ctesi/utils/ctesi-dl.sh
+++ ctesi/utils/ctesi-dl.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Please provide a url to download from'
+    exit 1
+fi
+
 url=$1
+
 filename=$(curl -JO "$url" | grep -o -E 'filename\s'\''(.*)'\''$' | sed -e 's/filename //' -e 's/'\''//g')
-unzip -q -d "${filename%*.zip}" "$filename"
-rm "$filename"
+
+if [[ $filename ]]; then
+    unzip -q -d "${filename%*.zip}" "$filename" && rm "$filename"
+else
+    echo 'File not found!'
+fi
