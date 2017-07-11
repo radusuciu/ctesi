f3484858b78d824a368ccf803473cdcb807a201f
Radu Suciu <radusuciu@gmail.com>
2017-07-10 20:34:54 -0700

    improve ctesi-dl shell script
    can now provide multiple urls, as well as numeric ids without full url

--- ctesi/utils/ctesi-dl.sh
+++ ctesi/utils/ctesi-dl.sh
@@ -3,16 +3,32 @@
 set -e
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Please provide a url to download from'
+    echo 'Please provide a url to download from.'
     exit 1
 fi
 
-url=$1
 
-filename=$(curl -JO "$url" | grep -o -E 'filename\s'\''(.*)'\''$' | sed -e 's/filename //' -e 's/'\''//g')
+dataset_url_root='http://titanic.scripps.edu/zip/'
 
-if [[ $filename ]]; then
-    unzip -q -d "${filename%*.zip}" "$filename" && rm "$filename"
-else
-    echo 'File not found!'
-fi
+
+for i in "$@"; do
+    # check if we are passed just a number
+    if ! [[ -n ${input//[0-9]/} ]]; then
+        i="${dataset_url_root}${i}"
+    fi
+
+    dl($i)
+done
+
+function dl() {
+    url=$1
+
+    filename=$(curl -JO "$url" | grep -o -E 'filename\s'\''(.*)'\''$' | sed -e 's/filename //' -e 's/'\''//g')
+
+    if [[ $filename ]]; then
+        unzip -q -d "${filename%*.zip}" "$filename" && rm "$filename"
+    else
+        echo 'File not found!'
+    fi
+}
+
